f1446fe196f06952e8d242a2cfb3e7dda95b4de4
Sergio Castillo <sergio.castillo5392@stu.mtec.edu>
2023-12-18 09:15:57 -0700

    new project

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('stuff'));
+
+app.get(`./car.html`, (req, res) => {
+    res.send('\ngot you\n');
+})
+
+app.get(/i*fly/, (req, res) => {
+    res.send('\nIt is true\n')
+})
+
+app.get(/i*apple$/, (req, res) => {
+    res.send(`\nWe have an apple for you\n`)
+})
+
+app.route('banana')
+.get((req, res) => {
+    res.send('get request\n')
+})
+.put((req, res) => {
+    res.send('get pu\n')
+})
+.delete((req, res) => {
+    res.send('get delete\n')
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`on port: ${port}`);
+})
